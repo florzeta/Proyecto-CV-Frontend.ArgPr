7562b779bf15ba323e0c6793475702037f36c2e7
florzeta <99660646+florzeta@users.noreply.github.com>
2023-02-28 18:28:32 -0300

    retoques finales | cerrar modal al enviar form

--- js/submit.js
+++ js/submit.js
@@ -1,4 +1,7 @@
 const $form = document.querySelector('#form')
+const nombre = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
 
 $form.addEventListener('submit', botonSubmit)
 
--- js/ventanaContacto.js
+++ js/ventanaContacto.js
@@ -2,6 +2,7 @@ const btnAbrirModal = document.getElementById("openBtn");
 const modal = document.getElementById("myModal");
 const cerrarModal = document.getElementById("closeBtn");
 var submitBtn = document.getElementById("submit-btn");
+const form = document.querySelector("form");
 
 
 // Cuando se hace clic en el botón, abrir el modal
@@ -21,15 +22,44 @@ window.onclick = function(event) {
   }
 }
 
-submitBtn.addEventListener("click", function() {
-  Swal.fire({
-    title: "¡Formulario enviado!",
-    text: "Gracias por contactarme, me pondré en contacto contigo lo antes posible.",
-    icon: "success",
-    confirmButtonColor: "#3085d6",
-    confirmButtonText: "Aceptar",
-    showClass: {
-      popup: 'animate__animated animate__fadeInDown'
-    },
-  });
-});
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  const nombre = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
+  
+  if (!nombre || !email || !mensaje) {
+    Swal.fire({
+      title: "Error",
+      text: "Por favor, completa todos los campos obligatorios.",
+      icon: "error",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Aceptar",
+      showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+    });
+
+  } else {
+    // Si todos los campos están completos, permitir que el formulario se envíe
+    form.submit();
+    
+    // Mostrar mensaje de éxito
+    Swal.fire({
+          title: "¡Formulario enviado!",
+          text: "Gracias por contactarme, me pondré en contacto contigo lo antes posible.",
+         icon: "success",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Aceptar",
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown'
+          },
+        });
+      // Cerrar el modal después de enviar el formulario
+    modal.style.display = "none";
+      }
+      });
+
+
+
